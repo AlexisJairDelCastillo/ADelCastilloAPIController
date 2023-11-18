b7044fbc7b5cd411289fe6a85fa517fb030a8e79
AlexisJairDelCastillo <adelcastillocastillo1@gmail.com>
2023-11-17 18:08:39 -0600

    Add project files.

---
+++ ML/Informacion.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ML
+{
+    public class Informacion
+    {
+        public List<People> People { get; set; }
+
+        public List<Inscriptions> Inscriptions { get; set; }
+    }
+
+    public class People
+    {
+        public string Name { get; set; }
+
+        public string LastName { get; set; }
+    }
+
+    public class Inscriptions
+    {
+        public int CourseId { get; set; }
+
+        public DateTime InscripcionDate { get; set; }
+
+        public DateTime CertificationDate { get; set; }
+
+        public int Advance { get; set; }
+
+        public decimal ScoreCourse { get; set; }
+
+        public string FolioCertificate { get; set; }
+
+        public bool AnyTest { get; set; }
+
+        public Course Course { get; set; }
+    }
+
+    public class Course
+    {
+        public string Name { get; set; }
+
+        public string ImageUrl { get; set; }
+
+        public Sector Sector { get; set; }
+    }
+
+    public class Sector
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string ColorTheme { get; set; }
+    }
+}
---
+++ PL/Controllers/PersonaController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace PL.Controllers
+{
+    public class PersonaController : Controller
+    {
+        public async Task<ActionResult> Persona()
+        {
+            string apiUrl = "https://besvc.capacitateparaelempleo.org/api/inscriptions/TestReport";
+
+            try
+            {
+                using (HttpClient client = new HttpClient())
+                {
+                    HttpResponseMessage response = await client.GetAsync(apiUrl);
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        string responseData = await response.Content.ReadAsStringAsync();
+
+                        var data = JsonConvert.DeserializeObject<ML.Informacion>(responseData);
+
+                        return View("Persona", data);
+                    }
+                    else
+                    {
+                        return View("Error");
+                    }
+                }
+            }catch(Exception ex)
+            {
+                return View("Error");
+            }
+        }
+    }
+}
